584c80944fb80cf9f809fcf7c570274236d70a55
Taylor C. Richberger <Taywee@gmx.com>
2015-10-14 17:17:13 -0600

    addlinkstotsd: use proper with block

--- addlinkstotsd.py
+++ addlinkstotsd.py
@@ -19,43 +19,35 @@ pathlist = list()
 files = dict()
 cur = None
 
-database = open(sys.argv[1], 'r')
+with open(sys.argv[1], 'r') as f:
+    for line in f.readlines():
+        stripline = line.rstrip()
 
-# Extracts current database into structure
-for line in database.readlines():
-    stripline = line.rstrip()
+        fileM = fileP.match(stripline)
+        attribM = attribP.match(stripline)
+        if fileM:
+            pathlist.append(fileM.group(1))
 
-    fileM = fileP.match(stripline)
-    attribM = attribP.match(stripline)
-    if fileM:
-        pathlist.append(fileM.group(1))
+            if fileM.group(1) not in files:
+                files[fileM.group(1)] = dict()
 
-        if fileM.group(1) not in files:
-            files[fileM.group(1)] = dict()
+            cur = files[fileM.group(1)]
+        elif attribM:
+            if attribM.group(1) not in cur:
+                cur[attribM.group(1)] = set()
 
-        cur = files[fileM.group(1)]
-    elif attribM:
-        if attribM.group(1) not in cur:
-            cur[attribM.group(1)] = set()
+            if len(attribM.group(2)) > 1:
+                cur[attribM.group(1)].update(attribM.group(2).split(','))
 
-        if len(attribM.group(2)) > 1:
-            cur[attribM.group(1)].update(attribM.group(2).split(','))
+with open(sys.argv[2], 'r') as f:
+    for line in f.readlines():
+        stripline = line.strip()
+        path, link = stripline.split(' ')
+        if path in files:
+            if 'symlinks' not in files[path]:
+                files[path]['symlinks'] = set()
 
-database.close()
-
-links = open(sys.argv[2], 'r')
-
-# Extracts current database into structure
-for line in links.readlines():
-    stripline = line.strip()
-    path, link = stripline.split(' ')
-    if path in files:
-        if 'symlinks' not in files[path]:
-            files[path]['symlinks'] = set()
-
-        files[path]['symlinks'].add(link)
-
-links.close()
+            files[path]['symlinks'].add(link)
 
 for path in pathlist:
     f = files[path]
