6a6fb4a6135fba0e7f41b55fd6a28124256fa19e
Taylor C. Richberger <Taywee@gmx.com>
2018-01-24 17:06:08 -0700

    fixes to makesnapshot.sh

--- makesnapshot.sh
+++ makesnapshot.sh
@@ -3,44 +3,54 @@
 # Written by Taylor C. Richberger <tcr@absolute-performance.com>
 # This is proprietary software.  No warranty, explicit or implicit, provided.
 
+log() {
+  now=$(date +'%Y-%m-%d %H:%M:%S')
+  echo "INFO [$now]: $*"
+}
+
 die() {
-  echo "ERROR: $*" >&2
+  now=$(date +'%Y-%m-%d %H:%M:%S')
+  echo "ERROR [$now]: $*" >&2
+  if [ -n "$dbresume" ]; then
+    log 'Emergency resuming DB'
+    eval "$dbresume"
+  fi
   exit 1
 }
 
 usage() {
   cat <<HERE
 $0 [options]
-    -h  Show this help
+    -h              Show this help
+    -C [certfile]   The certificate file for the storwizerest host
+    -M [mount]      The temporary mount location, where the LV will be created before remounting it
+    -P [command]    The db pause command
+    -R [command]    The db resume command
+    -c [file]       The credentials file, which are used to authenticate against the REST server
+    -d [disk]       The disk to use
+    -f [group]      The FlashCopy consistency group name
+    -m [mount]      The final mount location (usually data2)
+    -r [hostname]   The REST hostname; should match the certificate CN
+    -v [group]      The volume group, which will be removed and recreated during the process of this command
 HERE
 }
 
-getfield() {
-  filename="${1:?Need to specify the filename}"
-  field="${2:?Need to specify the field}"
-  awk "-vfield=$field" '{if (field == $1) print $2}' "$filename"
-}
-
-credfile=/etc/storwize.pass
-username=
-password=
-
-while getopts hu:p:c:C:d:v:m:P: opt; do
+while getopts hC:M:P:R:c:d:f:m:r:v: opt; do
   case $opt in
     h)
       usage
       exit
       ;;
-    u) username="$OPTARG" ;;
-    p) password="$OPTARG" ;;
-    c) credfile="$OPTARG" ;;
     C) certfile="$OPTARG" ;;
+    M) tempmount="$OPTARG" ;;
+    P) dbpause="$OPTARG" ;;
+    R) dbresume="$OPTARG" ;;
+    c) credentials="$OPTARG" ;;
     d) disk="$OPTARG" ;;
-    v) vgname="$OPTARG" ;;
-    P) prevmount="$OPTARG" ;;
+    f) fcname="$OPTARG" ;;
     m) mount="$OPTARG" ;;
     r) resthost="$OPTARG" ;;
-    g) groupname="$OPTARG" ;;
+    v) vgname="$OPTARG" ;;
     ?)
       usage
       exit 2
@@ -48,35 +58,56 @@ while getopts hu:p:c:C:d:v:m:P: opt; do
   esac
 done
 
-echo mount: ${mount:?Mount must be set}
-echo disk: ${disk:?Disk must be set}
-echo vgname: ${vgname:?VG name must be set}
-echo certfile: ${certfile:?Cert file must be set}
-echo resthost: ${resthost:?REST host must be set}
-echo groupname: ${groupname:?FC Consistency Group Name must be set}
+check_set() {
+  if [ -z "$1" ]; then
+    die "$2"
+  fi
+}
 
-if [ -z "$username"] || [ -z "$password" ]; then
-  username="$(getfield "$credfile" username)"
-  password="$(getfield "$credfile" password)"
-fi
+check_set "$certfile" 'certfile must be set'
+check_set "$tempmount" 'tempmount must be set'
+check_set "$credentials" 'credentials must be set'
+check_set "$disk" 'disk must be set'
+check_set "$fcname" 'fcname must be set'
+check_set "$mount" 'mount must be set'
+check_set "$resthost" 'resthost must be set'
+check_set "$vgname" 'vgname must be set'
 
-if [ -z "$username"] || [ -z "$password" ]; then
-  die 'Username or password not set'
-fi
+tempdata="$tempmount/data"
+
+export JBCRELEASEDIR=/jbase/cutools
+export JBCGLOBALDIR=$JBCRELEASEDIR
+export JBCJREDIR=$JBCRELEASEDIR/java/jre
+export JBCJRELIB=$JBCRELEASEDIR/java/JBCJRELIB:$JBCRELEASEDIR/java/jvmlib
+export PATH=$JBCRELEASEDIR/bin:$JBCRELEASEDIR/config:$PATH
+export LD_LIBRARY_PATH=$JBCRELEASEDIR/lib:$JBCJRELIB:/usr/lib:$LD_LIBRARY_PATH
+export PATH=$JBCRELEASEDIR/bin:$JBCJREDIR/bin:$PATH
+
+log 'Removing mount and destroying VG'
 
 umount -f "$mount"
 varyoffvg "$vgname"
 exportvg "$vgname"
 
-# TODO: make sure this works
-curl --fail --cacert "$certfile" -u "${username}:${password}" -G -d prep -d "fc_consist_group_name=${groupname}" "https://${resthost}/api/fcconsistgrp/start/" || die 'could not start consistency group'
+if [ -n "$dbpause" ]; then
+  log 'Pausing DB'
+  eval "$dbpause"
+fi
 
-chdev -l "$disk" -a pv=clear
-recreatevg -y "$vgname" -L"$mount" "$disk"
 
-if [ -n "$prevmount" ]; then
-  chfs -m"$mount" "$prevmount"
-end
+log 'Trying to stop the snap copy if it is in process.'
+curl --cacert "$certfile" --netrc-file "$credentials" -G -d "fc_consist_group_name=${fcname}" "https://${resthost}/api/fcconsistgrp/stop/" >/dev/null 2>&1
 
-fsck -y "$mount"
-mount "$mount"
+log 'Starting the snapshot'
+curl --fail --cacert "$certfile" --netrc-file "$credentials" -G -d prep -d "fc_consist_group_name=${fcname}" "https://${resthost}/api/fcconsistgrp/start/" || die 'Could not start consistency group'
+if [ -n "$dbresume" ]; then
+  log 'Resuming DB'
+  eval "$dbresume"
+fi
+
+log 'Recreating the vg and mounting it'
+chdev -l "$disk" -a pv=clear || die 'Could not chdev'
+recreatevg -y "$vgname" -l/etc/"$vgname".map -L"$tempmount" "$disk" || die 'Could not createvg'
+chfs -m"$mount" "$tempdata" || die 'Failed chfs'
+fsck -y "$mount" || die 'Failed fsck'
+mount "$mount" || die 'Could not mount'
